6dbd415200a32217f181f492f71247236c3d97a7
ammar10 <ammar.tariq@hotmail.com>
2018-04-18 16:51:57 +0500

    location setup but not working

--- android/app/src/main/java/com/rn_advanced/MainApplication.java
+++ android/app/src/main/java/com/rn_advanced/MainApplication.java
@@ -11,6 +11,8 @@ import com.oblador.vectoricons.VectorIconsPackage;
 import com.reactnativenavigation.NavigationApplication;
 import com.airbnb.android.react.maps.MapsPackage;
 
+ 
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -61,6 +63,8 @@ public class MainApplication extends NavigationApplication  {
           // eg. new VectorIconsPackage()
              new VectorIconsPackage(),
              new MapsPackage()
+
+
       );
   }
 
